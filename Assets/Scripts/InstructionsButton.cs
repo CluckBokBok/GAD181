d1ab9a14a7492e21fa8b991c3d019cdf092c1ebb
TaylahFrederickSAE <1032576@student.sae.edu.au>
2024-06-18 14:11:27 +1000

    Added Instructional UI and adjusted existing UI - for some reason everything got really big lol

---
+++ Assets/Scripts/InstructionsButton.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionsButton : MonoBehaviour
+{
+
+    public GameObject InstructionsPanel;
+    public bool InstructionsPanelActive = false; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+    public void MenuButtonPress()
+    {
+       
+            InstructionsPanel.SetActive(true);
+      
+
+
+    }
+
+    public void CloseButtonPress()
+    {
+       
+            InstructionsPanel.SetActive(false);
+       
+    }
+
+}
